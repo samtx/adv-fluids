9d01386ccc51691c76f988b03927fbfeb98770fc
Sam Friedman <samtx@outlook.com>
2017-03-30 02:28:55 +0000

    convert for f77. Not working

--- correlations/correlations.f
+++ correlations/correlations.f
@@ -1,43 +1,48 @@
-program correlations
-
-implicit none
-integer :: i, n, t, tau, max_lag
-real :: sum_num, sum_denom
-real, dimension(500) :: y1, y2
-real, dimension(500) :: rho, lags
-parameter, real :: pi = 3.141592653589793
-
-open(unit=1,file='output.dat',form='FORMATTED',action='WRITE')
-
-! Generate sine wave
-do t = 1, n
-    y1(t) = sin(float(t))
-end do
- 
-! Loop over tau
-do tau = 0, max_lag-1
- 
-    ! Shift the signal by tau
-    y2(:) = 0.0
-    y2(1:n-tau) = y1(tau:n)
-
-    ! Perform the correlations
-    sum_num = 0.0
-    sum_denom = 0.0
-    do i = 1,n
-        sum_num = sum_num + y1(i)*y2(i)
-        sum_denom = sum_denom + y1(i)*y1(i)
-    end do    
-    rho(tau+1) = sum_num / sum_denom
-    lags(tau+1) = tau
-    
-end do
-
-! Write results to file
-do i = 1,max_lag
-    write(unit=1, fmt=""(I6, F12.7)"") lags(i) rho(i)
-end do
-
-close(unit=1)
-
-end program
+        PROGRAM CORRELATIONS
+        
+        IMPLICIT NONE
+        INTEGER I, N, T, TAU, MAX_LAG
+        REAL SUM_NUM, SUM_DENOM
+        REAL Y1(500), Y2(500)
+        REAL RHO(500), LAGS(500)
+        REAL PI
+        
+        OPEN(UNIT=1,FILE='OUTPUT.TXT')
+        
+        PI = 3.141592653589793
+        MAX_LAG = 500
+        N = 500
+        
+C       GENERATE SINE WAVE
+        DO T = 1, N
+            Y1(T) = SIN(FLOAT(T))
+        END DO
+         
+C       LOOP OVER TAU
+        DO TAU = 0, MAX_LAG-1
+         
+C       SHIFT THE SIGNAL BY TAU
+        Y2(1:N) = 0.0
+        Y2(1:N-TAU) = Y1(TAU:N)
+        
+C       PERFORM THE CORRELATIONS
+            SUM_NUM = 0.0
+            SUM_DENOM = 0.0
+            DO I = 1,N
+                SUM_NUM = SUM_NUM + Y1(I)*Y2(I)
+                SUM_DENOM = SUM_DENOM + Y1(I)*Y1(I)
+            END DO    
+            RHO(TAU+1) = SUM_NUM / SUM_DENOM
+            LAGS(TAU+1) = TAU
+            
+            
+        END DO
+100     FORMAT(I6, F12.7)
+C       WRITE RESULTS TO FILE
+        DO I = 1,MAX_LAG
+            WRITE(UNIT=1, FMT=100) LAGS(I), RHO(I)
+        END DO
+        
+        CLOSE(UNIT=1)
+        
+        END PROGRAM
