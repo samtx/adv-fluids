ffcad88d6f6991c24253cf332bae57b673a54ca5
Sam Friedman <samfriedman@tamu.edu>
2017-04-17 20:10:33 +0000

    shocktube stuff

--- shocktube/shocktube.py
+++ shocktube/shocktube.py
@@ -3,9 +3,20 @@
 import numpy as np
 import Coolprop.Coolprop as CP
 
+R = 287.1   # gas constant for air [J/(mol*K)]
 
-def cp(p, rho):
+
+def get_cp(p, rho):
     return CP.PropsSI('CP0MOLAR', 'P', p, 'D', d, 'Air')  # get specific heat of enthalpy in J/(mol*K)
+
+def get_kappa(p, rho):
+    cp = get_cp(p, rho)
+    return cp/(cp - R)
+    
+def get_rho(p, T):
+    '''p[i-1,i+1]
+       T[i-1,i+1]'''
+    return (p[1]+p[0])/(R * (T[1]-T[0]))
 
 def main():
     
@@ -17,7 +28,6 @@ def main():
     S = 0.5     # width, [m]
     cf = 1      # coefficient of friction
     Dh = 1      # hydraulic diameter
-    R = 287.1   # gas constant for air [J/(mol*K)]
     
     # unknowns
     rho = np.zeros((L/dx, T/dt))   # density
@@ -25,12 +35,18 @@ def main():
     p = np.zeros((L/dx, T/dt))     # pressure
     v = np.zeros((L/dx, T/dt))     # velocity
     k = np.zeros((L/dx, T/dt))     # kappa (cp/(cp-R))
+    q = np.zeros((L/dx, T/dt))     # heat [kJ/kg] 
     
     # mass transient
-    m[i, t] = -1/dx * (m[i+1,t]*v[i+1,t] - m[i-1,t]*v[i-1,t] + (p[i+1,t] - p[i-1,t])*S) - cf * m[i,t]**2 / (2*Dh*rho[i,t]*S)
+    dmdt[i, t] = -1/dx * (m[i+1,t]*v[i+1,t] - m[i-1,t]*v[i-1,t] + (p[i+1,t] - p[i-1,t])*S) - cf * m[i,t]**2 / (2*Dh*rho[i,t]*S)
     
     # pressure transient
-    p[i, t] = -k/dx * ( m[i+1,t]*p[i+1,t]/(rho[i+1,t]*S) - m[i-1,t]*p[i-1,t]/(rho[i-1,t]*S) ) - 
+    dpdt[i, t] = -k[i,t]/dx * (m[i+1,t]*p[i+1,t]/(rho[i+1,t]*S) - m[i-1,t]*p[i-1,t]/(rho[i-1,t]*S)) - \
+        (k[i,t]-1) * (m[i,t]*q[i,t]/dV + cf*m[i+1,t]*m[i,t]**2/(2*Dh*S*rho[i,t]**2 * S**2)) - \
+        (k[i,t]-2) * m[i,t]/(rho[i,t]*(S**2))*(m[i,t]/(2*rho[i,t]*dx)*(m[i+1,t]/S - m[i-1,t]/S) + dmdt[i+1,t]) - \ 
+        (k[i,t]-2) / (2*dx) * (m[i+1,t]**3/(rho[i+1,t]**2 * S**3) - m[i-1,t]**3/(rho[i-1,t]**2 * S**3))
+              
+    
 
 
 if __name__ == "__main__":
