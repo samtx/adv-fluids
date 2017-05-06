d37207b52aaf7f4fa1cec30634a66a9bb47c53f1
Sam Friedman <samtx@outlook.com>
2017-05-06 15:47:22 -0500

    finalize plot making function. convert Pa to bar

--- shocktube/plotFoam.py
+++ shocktube/plotFoam.py
@@ -5,8 +5,8 @@ import sys
 import pandas as pd
 import csv
 import numpy as np
-# import matplotlib
-# matplotlib.use('cairo')
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
@@ -31,23 +31,43 @@ def get_plot_data(df, x0):
     df = df[df['x']==x0]  # restrict data to that spatial location
     print(df)
     df = df.sort_values(['t'])  # sort by increasing
+    df = df[df['t']<=0.01]  # restrict to tmax = 0.01
     ary = df.as_matrix(columns=['t','T','U','p'])
     t = ary[:,0]
     T = ary[:,1]
     U = ary[:,2]
-    p = ary[:,3]
+    p = ary[:,3] / 1.0e5  # convert Pa to bar
     return t, T, U, p
 
 
-def plot_data(t, T, U, p, x0):
-    plt.plot(t, p)
-    plt.xlabel('t', fontsize=22)
-    plt.ylabel('p', fontsize=22)
-    plt.title(r'Pressure over time at x={0}'.format(x0), fontsize=20)
-    plt.xticks(fontsize=16)
-    plt.yticks(fontsize=16)
-    plt.ylim([20000,100000])
-    plt.show()
+def single_plot(x, y, var_, imgFmt='.eps'):
+    opts = {
+        'p': {
+            'ylim': [np.min(y)*0.95,np.max(y)*1.05],
+            'ylabel': r'P [bar]'
+        },
+        'U': {
+            'ylim': [-5, np.max(y)*1.05],
+            'ylabel': r'|U| [m/s]'
+        },
+        'T': {
+            'ylim': [np.min(y)*0.95, np.max(y)*1.05],
+            'ylabel': r'T [K]'
+        }
+    }
+    varOpts = opts.get(var_)
+    plt.figure()
+    plt.plot(x, y, linewidth=2.0)
+    plt.xlabel('t [sec]', family='serif', fontsize=16)
+    plt.ylabel(varOpts['ylabel'], family='serif', fontsize=16)
+    # plt.title(r'Pressure over time at x={0}'.format(x0), fontsize=20)
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.grid(True)
+    plt.xlim([0,0.01])
+    plt.ylim(varOpts['ylim'])
+    # plt.show()
+    plt.savefig(var_+'_plot'+imgFmt)
 
 
 def foam2csv(dirPath, outPath):
@@ -74,62 +94,24 @@ def foam2csv(dirPath, outPath):
         data.to_csv(outFile, index=False)
     return outPath
 
-def convert_to_csv(ext='.xy', dir_=None):
-    if not dir_:
-        dir_ = os.getcwd()  # use default pwd
-    fileList = []
-    for root, dirs, filenames in os.walk(dir_):
-        for fname in filenames:
-            if fname.endswith(ext):
-                fileList.append(fname)
-                parse_text(os.path.join(root,fname))
-    return fileList
-
-def concatenate_files(ext='.xy', dir_=None, out_fname=None):
-    if not dir_:
-        dir_ = os.getcwd()  # use default pwd
-    fileList = []
-    if not out_fname:
-        out_fname = 'data.txt'
-    # outFile = open(os.path.join(dir_,out_fname),'w')
-    i = 0
-    for root, dirs, filenames in os.walk(dir_):
-        i += 1
-        print(i)
-        print(root)
-        print(dirs)
-        print(filenames)
-        for fname in filenames:
-            if fname.endswith(ext):
-                fileList.append(fname)
-                fpath = os.path.join(root, fname)
-                dirname = os.path.basename(os.path.dirname(fpath))
-                print(dirname)
-                # print('PATH=',fpath,'DIRNAME=',os.path.dirname(fpath),'BASENAME=',os.path.basename(fpath))
-                with open(fpath, 'r') as f:
-                    pass
-                    # outFile.write(f.read())
-    # outFile.close()
 
 if __name__ == "__main__":
-
-    main()
-    # return
-    # # ---- Extract time plots from data ----
-    # x0 = 0.995  # position to plot values over time
-    # csvPath = os.path.join(os.getcwd(),'data.csv')
-    # df = pd.read_csv(csvPath)
-    # t, T, U, p = get_plot_data(df, x0)
-    # plt.plot(t, p)
-    # plt.xlabel('t', fontsize=22)
-    # plt.ylabel('p', fontsize=22)
-    # plt.title(r'Pressure over time at x={0}'.format(x0), fontsize=20)
-    # plt.xticks(fontsize=16)
-    # plt.yticks(fontsize=16)
-    # plt.ylim([20000,100000])
-    # plt.show()
-    # # plt.savefig('plot.png')
-    # # print('t=',t)
-    # # print('T=',T)
-    # # print('U=',U)
-    # print('p=',p)
+    dir_ = os.getcwd()
+    csvFileName = 'data3.csv'
+    subdir = 'postProcessing/sample'
+    outPath = os.path.join(dir_,csvFileName)
+    dirPath = os.path.join(dir_,subdir)
+    csvPath = foam2csv(dirPath, outPath)
+    # ---- Extract time plots from data ----
+    x0 = 0.705  # position to plot values over time
+    csvPath = os.path.join(dir_,csvFileName)
+    df = pd.read_csv(csvPath)
+    t, T, U, p = get_plot_data(df, x0)
+    print('t=',t)
+    print('p=',p)
+    print('T=',T)
+    print('U=',U)
+    imgFmt = '.eps'
+    single_plot(t, p, 'p', imgFmt)
+    single_plot(t, T, 'T', imgFmt)
+    single_plot(t, U, 'U', imgFmt)
